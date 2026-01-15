97d13502a61920607923a91ce717d2dbed6c0907
Tr1stan21 <javichd21@gmail.com>
2026-01-15 10:34:33 +0100

    Base project structure

---
+++ src/main/java/com/tradetune/app/infrastructure/persistance/HibernateVehicleRepository.java
@@ -0,0 +1,6 @@
+package com.tradetune.app.infrastructure.persistance;
+
+import com.tradetune.app.domain.repository.VehicleRepository;
+
+public class HibernateVehicleRepository implements VehicleRepository {
+}
